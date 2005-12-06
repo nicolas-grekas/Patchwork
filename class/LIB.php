182d5944d12c9482b577974e0e86937f4c6b955d
IA <IA@28de78d2-576f-9a45-b889-8b31570dbf8a>
2005-12-06 21:24:29 +0000

    git-svn-id: https://svn.intellagence.eu/patchwork@75 28de78d2-576f-9a45-b889-8b31570dbf8a

--- class/LIB.php
+++ class/LIB.php
@@ -50,28 +50,62 @@ class LIB
 	);
 	protected static $ACCENT_FROM = array();
 	protected static $ACCENT_TO = array();
+	protected static $ACCENT_LENGTH;
+
+	protected static function initAccents()
+	{
+		self::$ACCENT_FROM = array('/[ÆǼ]/u', '/[æǽ]/u', '/ß/u', '/Œ/u', '/œ/u');
+		self::$ACCENT_TO   = array('AE'     , 'ae'     , 'ss'  , 'OE'  , 'oe'  );
+
+		$len = self::$ACCENT_LENGTH = count(self::$ACCENT);
+		for ($i = 0; $i < $len; ++$i)
+		{
+			$v = self::$ACCENT[$i];
+			self::$ACCENT_FROM[] = '/[' . substr($v, 1) . ']/u';
+			self::$ACCENT_TO[]   = $v{0};
+		}
+	}
 
 	/**
 	 * Removes all accents from an UTF-8 string, and optionnaly change it's case.
 	 */
 	public static function stripAccents($str, $case = 0)
 	{
-		if (!self::$ACCENT_FROM)
-		{
-			self::$ACCENT_FROM = array('/[ÆǼ]/u', '/[æǽ]/u', '/ß/u', '/Œ/u', '/œ/u');
-			self::$ACCENT_TO   = array('AE'     , 'ae'     , 'ss'  , 'OE'  , 'oe'  );
-
-			$len = count(self::$ACCENT);
-			for ($i = 0; $i < $len; ++$i)
-			{
-				$v = self::$ACCENT[$i];
-				self::$ACCENT_FROM[] = '/[' . substr($v, 1) . ']/u';
-				self::$ACCENT_TO[]   = $v{0};
-			}
-		}
+		if (!self::$ACCENT_FROM) self::initAccents();
 
 		$str = preg_replace(self::$ACCENT_FROM, self::$ACCENT_TO, $str);
 
 		return $case>0 ? mb_strtoupper($str) : $case<0 ? mb_strtolower($str) : $str;
+	}
+
+	/**
+	 * Transform a string to a RegExp that is not sentive to accents
+	 */
+	public static function getRxQuoteInsensitive($str, $delimiter = '')
+	{
+		if (!self::$ACCENT_FROM) self::initAccents();
+
+		$str = '' === $delimiter ? preg_quote($str) : preg_quote($str, $delimiter);
+
+		$str = preg_replace("/['’]/u", "['’]", $str);
+		$str = preg_replace("'[- _]'u", '[- _]', $str);
+		$str = preg_replace(self::$ACCENT_FROM, self::$ACCENT_TO, $str);
+
+		$len = self::$ACCENT_LENGTH;
+		for ($i = 0; $i < $len; ++$i)
+		{
+			$v = self::$ACCENT[$i];
+			$str = str_replace($v{0}, "[{$v}]", $str);
+		}
+
+		return $str;
+	}
+
+	/**
+	 * Return an alphabetic RexExp class, with accents support
+	 */
+	public static function getAlphaRx()
+	{
+		return 'a-zA-ZÆǼæǽßŒœ' . implode('', self::$ACCENT);
 	}
 }
