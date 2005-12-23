5ad7711d8efe8376adeea264210906c0650c6aaf
IA <IA@28de78d2-576f-9a45-b889-8b31570dbf8a>
2005-12-23 15:31:12 +0000

    git-svn-id: https://svn.intellagence.eu/patchwork@91 28de78d2-576f-9a45-b889-8b31570dbf8a

---
+++ class/pipe/urlize.php
@@ -0,0 +1,50 @@
+<?php
+
+class pipe_urlize
+{
+	static $mailrx = '/(\s)([-a-z0-9_\.\+]+)@([-a-z0-9]+(\.[-a-z0-9]+)+)/i';
+	static $url1rx = '/(\s)([a-z]+:\\/\\/[-a-z0-9_.!~*\'(),\\/?:@&=+$#]+)/i';
+	static $url2rx = '/(\s)([-a-z0-9_]+(\.[-a-z0-9_]+)+[-a-z0-9_.!~*\'(),\\/?:@&=+$#]*)/i';
+
+	static function php($string)
+	{
+		$string = ' ' . CIA::string($string);
+
+		$string = preg_replace(
+			self::$mailrx . 'u',
+			'$1<a href="mailto:$2[&#97;t]$3">$2<span style="display:none">@</span>&#64;$3</a>',
+			$string
+		);
+
+		$string = preg_replace(
+			self::$url1rx . 'u',
+			'$1<a href="$2" target="_blank">$2</a>',
+			$string
+		);
+
+		$string = preg_replace(
+			self::$url2rx . 'u',
+			'$1<a href="http://$2" target="_blank">$2</a>',
+			$string
+		);
+
+		return substr($string, 1);
+	}
+
+	static function js()
+	{
+		?>/*<script>*/
+
+P<?php echo substr(__CLASS__, 5)?> = function($string)
+{
+	return (' ' + str($string)).replace(
+		<?php echo self::$mailrx?>g, '$1<a href="mailto:$2@$3">$2@$3</a>'
+	).replace(
+		<?php echo self::$url1rx?>g, '$1<a href="$2" target="_blank">$2</a>'
+	).replace(
+		<?php echo self::$url2rx?>g, '$1<a href="http://$2" target="_blank">$2</a>'
+	).substr(1);
+}
+
+<?php	}
+}
