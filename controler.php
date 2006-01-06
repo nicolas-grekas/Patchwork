d8411033e5f969dc036644e4c2ede84ac621d8c3
IA <IA@28de78d2-576f-9a45-b889-8b31570dbf8a>
2006-01-06 20:07:59 +0000

    Better handing of 304/Not modified for controler.php
    git-svn-id: https://svn.intellagence.eu/patchwork@102 28de78d2-576f-9a45-b889-8b31570dbf8a

--- controler.php
+++ controler.php
@@ -13,43 +13,51 @@ $contentType = array(
 
 $contentType = $contentType[$path];
 
-$path1 = explode(PATH_SEPARATOR, get_include_path());
-$len = count($path1);
+$path = explode(PATH_SEPARATOR, get_include_path());
+$len = count($path);
 
 $lang = '/' . CIA_LANG . '/';
 
 for ($i = 0; $i < $len; ++$i)
 {
-	switch (substr($path1[$i], -1))
+	switch (substr($path[$i], -1))
 	{
 		case '':
 		case '\\':
 		case '/': break;
-		default: $path1[$i] .= DIRECTORY_SEPARATOR;
+		default: $path[$i] .= DIRECTORY_SEPARATOR;
 	}
 
-	$path1[$i] .= 'public';
+	$path[$i] .= 'public';
 
-	if (file_exists($path1[$i] . $lang . $agent))
+	if (file_exists($path[$i] . $lang . $agent))
 	{
-		$path1[$i] .= $lang . $agent;
+		$path[$i] .= $lang . $agent;
 		break;
 	}
-	else $path1[$i] .= "/__/{$agent}";
+	else $path[$i] .= "/__/{$agent}";
 }
 
 if ($i == $len)
 {
 	$lang = '/__/';
-	for ($i = 0; $i < $len; ++$i) if (file_exists($path1[$i])) break;
+	for ($i = 0; $i < $len; ++$i) if (file_exists($path[$i])) break;
 }
 
 if ($i < $len)
 {
+	$path = $path[$i];
+
 	CIA::header('Content-Type: ' . $contentType);
 	CIA::setMaxage(-1);
 	CIA::setExpires(true);
-	readfile($path1[$i]);
+	CIA::writeWatchTable('public/static', 'tmp/');
+
+	$i = stat($path);
+	echo $i[1], '-', $i[7], '-', $i[9];
+	ob_end_clean();
+	
+	readfile($path[$i]);
 }
 
 exit;
