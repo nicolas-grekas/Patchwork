9972e420c8ed119b188c448deb4f6faa97c7a201
Nicolas Grekas <nicolas.grekas@gmail.com>
2006-01-09 09:32:49 +0000

    Enhanced behavior of QSelect when clicking on the arrow button Added pipe/trim.php

---
+++ class/pipe/trim.php
@@ -0,0 +1,20 @@
+<?php
+
+class pipe_trim
+{
+	static function php($a)
+	{
+		return trim( CIA::string($a) );
+	}
+
+	static function js()
+	{
+		?>/*<script>*/
+
+P<?php echo substr(__CLASS__, 5)?> = function($a)
+{
+	return str($a).replace(/^\s+/, '').replace(/\s+$/, '');
+}
+
+<?php	}
+}
