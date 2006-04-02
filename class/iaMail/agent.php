3497fbd15f91e2f68c3eb9dfd130dd45c14bff85
IA <IA@28de78d2-576f-9a45-b889-8b31570dbf8a>
2006-04-02 10:38:15 +0000

    - removed parsing for css url() images, not supported by mail client
    git-svn-id: https://svn.intellagence.eu/patchwork@224 28de78d2-576f-9a45-b889-8b31570dbf8a

--- class/iaMail/agent.php
+++ class/iaMail/agent.php
@@ -27,10 +27,7 @@ class iaMail_agent extends iaMail
 
 		CIA::__LANG__($lang);
 
-		$html = preg_replace_callback('/(\s)(src|background)\s*(=)\s*(["\'])?((?(4)[^(?:\4)]*|[^\s>]*)\.(jpe?g|png|gif))(?(4)\4)/iu', array($this, 'addRawImage'), $html);
-
-		// Limited support for http://www.w3.org/TR/REC-CSS2/syndata.html#uri
-		$html = preg_replace_callback("/([\s:])(url)(\()\s*([\"'])?([^\n\r]*\.(jpe?g|png|gif))(?(4)\4)\s*(\))/iu", array($this, 'addRawImage'), $html);
+		$html = preg_replace_callback('/(\s)(src|background)\s*=\s*(["\'])?((?(3)[^(?:\3)]*|[^\s>]*)\.(jpe?g|png|gif))(?(3)\3)/iu', array($this, 'addRawImage'), $html);
 
 		$this->setHTMLBody($html);
 		$this->setTXTBody( CONVERT::data($html, 'html', 'txt') );
@@ -40,33 +37,33 @@ class iaMail_agent extends iaMail
 
 	protected function addRawImage($match)
 	{
-		$url = $match[5];
+		$url = $match[4];
+
+		if (!preg_match("'^(ftp|https?)://'iu", $url)) 
+		{
+			if ('/' != substr($url, 0, 1)) $url = CIA::__ROOT__() . $url;
+
+			$url = CIA::__HOST__() . $url;
+		}
 
 		if (isset(self::$imageCache[$url])) $data =& self::$imageCache[$url];
 		else
 		{
-			if (!preg_match("'^(ftp|https?)://'iu", $url)) 
-			{
-				if ('/' != substr($url, 0, 1)) $url = CIA::__ROOT__() . $url;
-
-				$url = CIA::__HOST__() . $url;
-			}
-
 			$data = file_get_contents($url);
-			self::$imageCache[ $match[5] ] =& $data;
+			self::$imageCache[$url] =& $data;
 		}
 
-		switch (strtolower($match[6]))
+		switch (strtolower($match[5]))
 		{
 			case 'png': $mime = 'image/png'; break;
 			case 'gif': $mime = 'image/gif'; break;
 			default: $mime = 'image/jpeg';
 		}
 
-		$this->addHtmlImage($data, $mime, $match[5], false);
+		$this->addHtmlImage($data, $mime, $match[4], false);
 
 		$a =& $this->_html_images[ count($this->_html_images) - 1 ];
 
-		return $match[1] . $match[2] . $match[3] . 'cid:' . $a['cid'] . @$match[7];
+		return $match[1] . $match[2] . '=cid:' . $a['cid'];
 	}
 }
