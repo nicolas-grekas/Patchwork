491f1048cb3c9c64702ddcf53392ae04506e0690
IA <IA@28de78d2-576f-9a45-b889-8b31570dbf8a>
2006-04-07 11:38:19 +0000

    * removed some dependency on php.ini's error_log directive
    git-svn-id: https://svn.intellagence.eu/patchwork@238 28de78d2-576f-9a45-b889-8b31570dbf8a

--- error_handler.php
+++ error_handler.php
@@ -49,7 +49,8 @@ if (!$msg) switch ($code)
 }
 
 $cid = CIA::uniqid();
-$i = fopen(ini_get('error_log'), 'ab');
+$i = ini_get('error_log');
+$i = fopen($i ? $i : './tmp/error.log', 'ab');
 fwrite($i, "<a href='javascript:;' onclick=\"var a=document.getElementById('{$cid}');a.style.display=a.style.display?'':'none';\">$msg</a> in <b>$file</b> line <b>$line</b>:\n$message<blockquote id='{$cid}' style='display: none'>Context : $context</blockquote><br><br>");
 fclose($i);
 
