333f791d62cf30bf341f6f79e6f463f674ef96d2
IA <IA@28de78d2-576f-9a45-b889-8b31570dbf8a>
2006-06-11 20:46:19 +0000

    git-svn-id: https://svn.intellagence.eu/patchwork@388 28de78d2-576f-9a45-b889-8b31570dbf8a

--- language.php
+++ language.php
@@ -4,7 +4,6 @@ header('Content-Type: text/html; charset=UTF-8');
 header('Expires: ' . gmdate('D, d M Y H:i:s', CIA_TIME + CIA_MAXAGE) . ' GMT');
 header('Cache-Control: max-age=' . CIA_MAXAGE .',public');
 header('Vary: Accept-Language', false);
-setcookie('JS', '0', 2147364847, '/');
 
 function HTTP_Best_Language($supported)
 {
@@ -44,6 +43,4 @@ $lang = htmlspecialchars($lang);
 ?><html><head><title>...</title><script type="text/javascript">/*<![CDATA[*/
 if(window.Error&&navigator.userAgent.indexOf('Safari')<0)document.cookie='JS=1; path=/',document.cookie='JS=1; expires=Sun, 17-Jan-2038 19:14:07 GMT; path=/'
 location.replace("<?php echo $lang?>")
-/*]]>*/</script><meta http-equiv="refresh" content="0; URL=<?php echo $lang?>" /></head><body><a href="<?php echo $lang?>"><?php echo $lang?></a></body></html><?php
-
-exit;
+/*]]>*/</script><meta http-equiv="refresh" content="0; URL=<?php echo $lang?>" /></head><body><a href="<?php echo $lang?>"><?php echo $lang?></a></body></html>
