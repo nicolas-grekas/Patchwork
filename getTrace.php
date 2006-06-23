a99d2197f9db8e3e3396cfe017b51eb341762e22
IA <IA@28de78d2-576f-9a45-b889-8b31570dbf8a>
2006-06-23 10:53:57 +0000

    + resolveAgentTrace can now do request its requests through the shell rather than http
    git-svn-id: https://svn.intellagence.eu/patchwork@413 28de78d2-576f-9a45-b889-8b31570dbf8a

---
+++ getTrace.php
@@ -0,0 +1,19 @@
+<?php
+
+// Shell script to get one agent's trace
+// Setup the context needed to simulate a keys query
+
+$_SERVER['CIA_HOME'] = $_SERVER['argv'][2];
+$_SERVER['CIA_LANG'] = $_SERVER['argv'][3];
+$_SERVER['CIA_REQUEST'] = $_SERVER['argv'][4];
+
+if ($_SERVER['argv'][5]) $_SERVER['HTTPS'] = 'on';
+
+$_SERVER['REQUEST_METHOD'] = 'GET';
+$_SERVER['REQUEST_URI'] = implode($_SERVER['CIA_LANG'], explode('__', $_SERVER['CIA_HOME'], 2));
+$_SERVER['REQUEST_URI'] = preg_replace("'^https?://[^/]*'i", '', $_SERVER['REQUEST_URI']) . '?k$=';
+$_SERVER['QUERY_STRING'] = 'k$=';
+
+$_GET = array('k$' => '');
+
+include $_SERVER['argv'][1];
