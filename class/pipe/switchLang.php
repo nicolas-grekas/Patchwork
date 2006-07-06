0e78b0a7818a3f13af8bdf62bf0ab7f521aee8ba
IA <IA@28de78d2-576f-9a45-b889-8b31570dbf8a>
2006-07-06 09:48:01 +0000

    + added pipe_switchLang
    git-svn-id: https://svn.intellagence.eu/patchwork@443 28de78d2-576f-9a45-b889-8b31570dbf8a

---
+++ class/pipe/switchLang.php
@@ -0,0 +1,24 @@
+<?php
+
+class pipe_switchLang
+{
+	static function php($g, $lang)
+	{
+		$url = explode("/{$g->__LANG__}/", $g->__URI__, 2);
+		$url = implode("/{$lang}/", $url);
+
+		return $url;
+	}
+
+	static function js()
+	{
+		?>/*<script>*/
+
+P$<?php echo substr(__CLASS__, 5)?> = function($g, $lang)
+{
+	return $g.__URI__.replace(new RegExp('/' + $g.__LANG__ + '/'), '/' + $lang + '/');
+}
+
+<?php   }
+}
+
