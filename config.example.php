9d9fe01e83e1e41a678c244441f864ea049a53d0
grekas20 <grekas20@28de78d2-576f-9a45-b889-8b31570dbf8a>
2006-10-04 18:03:30 +0000

    ++ Multiple Application Inheritance implemented using C3 Method Resolution Order
    git-svn-id: https://svn.intellagence.eu/patchwork@521 28de78d2-576f-9a45-b889-8b31570dbf8a

--- config.example.php
+++ config.example.php
@@ -1,24 +1,24 @@
 <?php
 
-$CONFIG += array(		// Config parameters
+$CONFIG = array(
+
+	// For multiple inheritance, put an array in $CONFIG['extends'].
+	// If not set, defaults to "../../config.php"
+	// Uses C3 Method Resolution Order, like in Python 2.3.
+	'extends' => false,
 
 	'DEBUG_KEYS' => array('' => 1),	// password => debug_level hash
 
+	'use_path_info' => true,
 	'inheritance_optimization' => 'inline', // 'inline', 'include' or false
 	'timezone' => 'Europe/Paris',
-	'php' => 'c:/progra~1/wamp/php/php.exe', // Path to your php executable.
+	'php' => 'c:/progra~1/wamp/php/php.exe', // Path to your php (CLI) executable.
 	'debug_email' => 'webmaster',
 
 	'maxage' => 3600,
 	'lang_list' => 'fr',
 
-	'allow_debug' => true,
-
 	'translate_driver' => 'default_',
 	'translate_params' => array(),
 
 );
-
-if (!isset($CONFIG['DEBUG'])) $CONFIG['DEBUG'] = (int) @$CONFIG['DEBUG_KEYS'][(string)$_COOKIE['DEBUG']];
-
-CIA_GO(__FILE__, true); // 2nd parameter: true if your server supports PATH_INFO, else false 
