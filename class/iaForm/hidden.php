866430f6d4c59acee88f0c0c871867828b841587
Nicolas Grekas <nicolas.grekas@gmail.com>
2007-01-15 09:20:20 +0000

    * moved runtime DEBUG conditions to preprocessor

--- class/iaForm/hidden.php
+++ class/iaForm/hidden.php
@@ -153,14 +153,13 @@ class extends loop_callAgent
 		else if ($onerror && $this->status===false) return $this->errormsg = $onerror;
 		else if ($this->status===false)
 		{
-			if (DEBUG)
-			{
-				E('Input validation error in ' . get_class($this) . ' element: ' . print_r(array(
-					'name' => $this->name,
-					'value' => $this->value,
-					'valid' => $this->valid, $this->valid_args
-				), true));
-			}
+#>>>
+			E('Input validation error in ' . get_class($this) . ' element: ' . print_r(array(
+				'name' => $this->name,
+				'value' => $this->value,
+				'valid' => $this->valid, $this->valid_args
+			), true));
+#<<<
 
 			return "Input validation error in field {$this->name}";
 		}
