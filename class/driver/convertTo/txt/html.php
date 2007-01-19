caa3a1d7290116e3153ab38c46421369531ab6c3
grekas20 <grekas20@28de78d2-576f-9a45-b889-8b31570dbf8a>
2007-01-19 13:57:18 +0000

    - fixed UTF8 things with w3m's html to txt conversion * added auto setting of Reply-To header to iaMail_mime
    git-svn-id: https://svn.intellagence.eu/patchwork@649 28de78d2-576f-9a45-b889-8b31570dbf8a

--- class/driver/convertTo/txt/html.php
+++ class/driver/convertTo/txt/html.php
@@ -17,7 +17,11 @@ class extends driver_convertTo_abstract
 	function file($file)
 	{
 		$file = escapeshellarg($file);
-		return `w3m -dump -cols 80 -T text/html -I UTF-8 -O UTF-8 {$file}`;
+		$file = `w3m -dump -cols 80 -T text/html -I UTF-8 -O UTF-8 {$file}`;
+
+		if (false !== strpos($file, '━')) $file = str_replace('━', '_', $file);
+
+		return $file;
 	}
 
 }
