e631fba8a8ac8d09275bd6535ad7f318f5354180
grekas20 <grekas20@28de78d2-576f-9a45-b889-8b31570dbf8a>
2007-02-04 22:56:55 +0000

    * moved php.ini things to config.example.php
    git-svn-id: https://svn.intellagence.eu/patchwork@702 28de78d2-576f-9a45-b889-8b31570dbf8a

--- CIA.php
+++ CIA.php
@@ -14,63 +14,6 @@
 
 define('CIA', microtime(true)); isset($_SERVER['REQUEST_TIME']) || $_SERVER['REQUEST_TIME'] = time();
 
-// {{{ Server configuration
-
-if (!(isset($CONFIG['php_ini_is_ok']) && $CONFIG['php_ini_is_ok']))
-{
-	if (get_magic_quotes_gpc())
-	{
-		if (ini_get('magic_quotes_sybase')) { function _q_(&$a) {is_array($a) ? array_walk($a, '_q_') : $a = str_replace("''", "'", $a);} }
-		else { function _q_(&$a) {is_array($a) ? array_walk($a, '_q_') : $a = stripslashes($a);} }
-		_q_($_GET);_q_($_POST);_q_($_COOKIE);
-	}
-
-	ini_set('log_errors', true);
-	ini_set('display_errors', false);
-	ini_set('zlib.output_compression', false);
-	set_magic_quotes_runtime(0);
-}
-
-/* Copy/Paste this block at the end of your php.ini,
- * then set $CONFIG['php_ini_is_ok']=true in your global config.php
-
-; Replace this to your needs
-error_log = /tmp/php_error.log
-
-log_errors = On
-display_errors = Off
-zlib.output_compression = Off
-magic_quotes_gpc = Off
-magic_quotes_runtime = Off
-
-variables_order = "GPCES"
-register_globals = Off
-register_long_arrays = Off
-register_argc_argv = Off
-auto_globals_jit = On
-
-session.auto_start = Off
-session.use_only_cookies = On
-session.use_cookies = Off
-session.use_trans_sid = Off
-
-mbstring.language = neutral
-mbstring.script_encoding = UTF-8
-mbstring.internal_encoding = UTF-8
-
-mbstring.encoding_translation = On
-mbstring.detect_order = auto
-mbstring.http_input = auto
-mbstring.http_output = pass
-
-mbstring.substitute_character = none
-
-; String's functions overloading prevents binary use of a string. Use mb_* functions instead
-mbstring.func_overload = 0
-
-*/
-// }}}
-
 // {{{ Global context setup
 
 // $_REQUEST is an open door to security problems.
--- config.example.php
+++ config.example.php
@@ -1,5 +1,62 @@
 <?php
 
+/* If possible, copy/paste these directives at the end of your php.ini :
+
+; Replace this to your needs
+error_log = /tmp/php_error.log
+
+log_errors = On
+display_errors = Off
+zlib.output_compression = Off
+magic_quotes_gpc = Off
+magic_quotes_runtime = Off
+
+variables_order = "GPCES"
+register_globals = Off
+register_long_arrays = Off
+register_argc_argv = Off
+auto_globals_jit = On
+
+session.auto_start = Off
+session.use_only_cookies = On
+session.use_cookies = Off
+session.use_trans_sid = Off
+
+mbstring.language = neutral
+mbstring.script_encoding = UTF-8
+mbstring.internal_encoding = UTF-8
+
+mbstring.encoding_translation = On
+mbstring.detect_order = auto
+mbstring.http_input = auto
+mbstring.http_output = pass
+
+mbstring.substitute_character = none
+
+; String's functions overloading prevents binary use of a string. Use mb_* functions instead
+mbstring.func_overload = 0
+
+*/
+
+/* If your server's php.ini is ok, commment this section */
+
+if (get_magic_quotes_gpc())
+{
+	if (ini_get('magic_quotes_sybase')) { function _q_(&$a) {is_array($a) ? array_walk($a, '_q_') : $a = str_replace("''", "'", $a);} }
+	else { function _q_(&$a) {is_array($a) ? array_walk($a, '_q_') : $a = stripslashes($a);} }
+	_q_($_GET);_q_($_POST);_q_($_COOKIE);
+}
+
+ini_set('log_errors', true);
+ini_set('display_errors', false);
+ini_set('zlib.output_compression', false);
+set_magic_quotes_runtime(false);
+
+/**/
+
+
+/* Configure your globals CIA settings */
+
 $CONFIG += array(
 
 	'DEBUG' => 1,
