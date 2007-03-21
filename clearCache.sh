72e5d767fde0dae542ee1f1f5f7e8df3671841f0
grekas20 <grekas20@28de78d2-576f-9a45-b889-8b31570dbf8a>
2007-03-21 15:00:11 +0000

    git-svn-id: https://svn.intellagence.eu/patchwork@843 28de78d2-576f-9a45-b889-8b31570dbf8a

--- clearCache.sh
+++ clearCache.sh
@@ -5,13 +5,13 @@ find -name ".*.zcache.php" -exec rm -f {} \;
 find -name config.php -exec touch {} \;
 find -name ".config.zcache.php" -exec rm -f {} \;
 
-for I in `find -name zcache -type d`
+for I in `find -name zcache -type d 2> /dev/null`
 do
 	for J in `find $I/?/? -maxdepth 1 -type f -name "*.watch.*.php" 2> /dev/null`
 	do
 		echo "<?php @include '$J';" | php -q
 	done
 
-	find $I/?/? -maxdepth 1 -type f -exec rm -f {} \;
+	find $I/?/? -maxdepth 1 -type f -exec rm -f {} \; 2> /dev/null
 done
 
