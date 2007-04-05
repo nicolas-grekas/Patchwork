c9ef01cd69e95da247563a7aadc125dc50f01ef0
grekas20 <grekas20@28de78d2-576f-9a45-b889-8b31570dbf8a>
2007-04-05 18:15:19 +0000

    - added xmlns and xml:lang
    git-svn-id: https://svn.intellagence.eu/patchwork@916 28de78d2-576f-9a45-b889-8b31570dbf8a

--- class/CIA/clientside.php
+++ class/CIA/clientside.php
@@ -36,7 +36,7 @@ class extends CIA
 
 			echo $a =<<<EOHTML
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html lang="{$lang}">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="{$lang}" lang="{$lang}">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <script type="text/javascript" name="w$">/*<![CDATA[*/a=[{$agent},[{$a}],{$CIApID}]//]]></script>
 <script type="text/javascript" src="{$base}js/w?{$CIApID}"></script>
