ac0873f7a8606f2632b87f1d66dabe9613cb37c1
grekas20 <grekas20@28de78d2-576f-9a45-b889-8b31570dbf8a>
2007-04-17 10:36:35 +0000

    * partially split code in pieces
    git-svn-id: https://svn.intellagence.eu/patchwork@979 28de78d2-576f-9a45-b889-8b31570dbf8a

--- example/config.php
+++ example/config.php
@@ -8,14 +8,6 @@ $CONFIG += array(
 #	'DEBUG_ALLOWED'  => 1,
 #	'DEBUG_PASSWORD' => '',
 
-	// Used by iaMail in test mode
-#	'debug_email' => 'webmaster',
-
-	// Defaults for PEAR's Mail_mime
-#	'email_backend' => 'mail',
-#	'email_options' => '',
-
-
 	// Enable browser-side page rendering when available ?
 #	'clientside' => true,
 
