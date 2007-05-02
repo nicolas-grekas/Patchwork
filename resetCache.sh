89ac381b08cc46e2871a3a4f3d5e0f528a984ad8
grekas20 <grekas20@28de78d2-576f-9a45-b889-8b31570dbf8a>
2007-05-02 00:34:25 +0000

    * previous commit did rename text/serverside-html to text/server-html * optimized resetCache.sh
    git-svn-id: https://svn.intellagence.eu/patchwork@1044 28de78d2-576f-9a45-b889-8b31570dbf8a

--- resetCache.sh
+++ resetCache.sh
@@ -6,12 +6,14 @@ find -name config.cia.php -exec touch {} \;
 
 for I in `find -name zcache -type d 2> /dev/null`
 do
-	for J in `find $I/?/? -maxdepth 1 -type f -name "*.watch.*.php" 2> /dev/null`
+	mv $I $I.old
+
+	for J in `find $I.old/?/? -maxdepth 1 -type f -name "*.watch.*.php" 2> /dev/null`
 	do
 		echo "<?php @include '$J';" | php -q
 	done
 
-	find $I/?/? -maxdepth 1 -type f -exec rm -f {} \; 2> /dev/null
+	rm -Rf $I.old
 done
 
 find -name ".config.zcache.php" -exec rm -f {} \;
