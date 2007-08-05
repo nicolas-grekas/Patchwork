e99c5fe84ecbdb01e3828aad767f14d4f4d91fb5
nicolas.grekas <nicolas.grekas@28de78d2-576f-9a45-b889-8b31570dbf8a>
2007-08-05 10:52:43 +0000

    * refactored iaCron for periodic tasks and crontab
    git-svn-id: https://svn.intellagence.eu/patchwork@1159 28de78d2-576f-9a45-b889-8b31570dbf8a

---
+++ pieces/iaCron/class/iaCron/hourly.php
@@ -0,0 +1,22 @@
+<?php /*********************************************************************
+ *
+ *   Copyright : (C) 2006 Nicolas Grekas. All rights reserved.
+ *   Email     : nicolas.grekas+patchwork@espci.org
+ *   License   : http://www.gnu.org/licenses/gpl.txt GNU/GPL, see COPYING
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+
+
+class extends iaCron_periodic
+{
+	protected $crontab = '0 * * * *';
+
+	function execute()
+	{
+	}
+}
