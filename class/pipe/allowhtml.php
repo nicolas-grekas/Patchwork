7cd85594850d27c426b7fdffedea195f54710593
nicolas.grekas <nicolas.grekas@28de78d2-576f-9a45-b889-8b31570dbf8a>
2007-08-07 18:26:51 +0000

    * enabled url vectorization in HTML raw data
    git-svn-id: https://svn.intellagence.eu/patchwork@1169 28de78d2-576f-9a45-b889-8b31570dbf8a

--- class/pipe/allowhtml.php
+++ class/pipe/allowhtml.php
@@ -21,8 +21,8 @@ class
 		return (string) $string === (string) ($string-0)
 			? $string
 			: str_replace(
-				array('&#039;', '&quot;', '&gt;', '&lt;', '&amp;'),
-				array("'"     , '"'     , '>'   , '<'   , '&'    ),
+				array('&#039;', '&quot;', '&gt;', '&lt;', '&amp;', '{/}'                , '{~}'),
+				array("'"     , '"'     , '>'   , '<'   , '&'    , patchwork::__HOST__(), patchwork::__BASE__()),
 				$string
 			);
 	}
@@ -33,10 +33,12 @@ class
 
 P$allowhtml = function($string)
 {
+	var $base = base();
+
 	$string = str($string);
 	return (''+$string/1==$string)
 		? $string/1
-		: unesc($string);
+		: unesc($string).replace(/{\/}/g, $base.substr(0, $base.indexOf('/', 8)+1)).replace(/{~}/g, $base);
 }
 
 <?php	}
