6eaadf611e62af69633f41f7ccea47ecb0c173b4
nicolas.grekas <nicolas.grekas@28de78d2-576f-9a45-b889-8b31570dbf8a>
2007-09-02 17:06:08 +0000

    * missing license header
    git-svn-id: https://svn.intellagence.eu/patchwork@1227 28de78d2-576f-9a45-b889-8b31570dbf8a

--- preconfig.php
+++ preconfig.php
@@ -1,4 +1,15 @@
-<?php
+<?php /*********************************************************************
+ *
+ *   Copyright : (C) 2007 Nicolas Grekas. All rights reserved.
+ *   Email     : p@tchwork.org
+ *   License   : http://www.gnu.org/licenses/gpl.txt GNU/GPL
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
 
 
 
