5012e628af363622127640abe9bd2d7144bc5c4a
espci.org <espci.org@28de78d2-576f-9a45-b889-8b31570dbf8a>
2007-11-25 20:10:36 +0000

    git-svn-id: https://svn.intellagence.eu/patchwork@1288 28de78d2-576f-9a45-b889-8b31570dbf8a

--- pieces/pMail/class/Mail/mime.php
+++ pieces/pMail/class/Mail/mime.php
@@ -23,8 +23,8 @@ class extends self
 		foreach ($input as &$hdr_value)
 		{
 			$this->optimizeCharset($hdr_value, 'head');
-			$hdr_value = preg_replace("/[\r\n](?!\s)/", '$0 '); // Header injection protection
-			$hdr_value = str_replace('=?', "={$this->_eol} ?"); // Encoded string injection protection
+			$hdr_value = preg_replace("/[\r\n](?!\s)/", '$0 ', $hdr_value); // Header injection protection
+			$hdr_value = str_replace('=?', "={$this->_eol} ?", $hdr_value); // Encoded string injection protection
 			$hdr_value = preg_replace_callback(
 				"/{$ns}(?:[\\x80-\\xFF]{$ns})+/",
 				array($this, '_encodeHeaderWord'),
