c518e36674d557c4fc49c6841907b6c0bb6a7017
nicolas.grekas <nicolas.grekas@28de78d2-576f-9a45-b889-8b31570dbf8a>
2007-11-29 13:34:43 +0000

    + finalized iconv PHP implementation
    git-svn-id: https://svn.intellagence.eu/patchwork@1298 28de78d2-576f-9a45-b889-8b31570dbf8a

--- preconfig.php
+++ preconfig.php
@@ -329,13 +329,13 @@ define('UTF8_BOM', /*<*/__patchwork_loader::UTF8_BOM/*>*/);
 /*#>*/if (function_exists('iconv'))
 /*#>*/{
 /*#>*/	if ('UTF-8' !== iconv_get_encoding('input_encoding'))
-			iconv_set_encoding('input_encoding'   , 'UTF-8');
+			iconv_set_encoding('input_encoding'   , 'UTF-8//IGNORE');
 
 /*#>*/	if ('UTF-8' !== iconv_get_encoding('internal_encoding'))
-			iconv_set_encoding('internal_encoding', 'UTF-8');
+			iconv_set_encoding('internal_encoding', 'UTF-8//IGNORE');
 
 /*#>*/	if ('UTF-8' !== iconv_get_encoding('output_encoding'))
-			iconv_set_encoding('output_encoding'  , 'UTF-8');
+			iconv_set_encoding('output_encoding'  , 'UTF-8//IGNORE');
 /*#>*/}
 
 
