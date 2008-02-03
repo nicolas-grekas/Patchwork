7fd9d93303bce3facac30056ad5783ea40049b57
nicolas.grekas <nicolas.grekas@28de78d2-576f-9a45-b889-8b31570dbf8a>
2008-02-03 23:51:34 +0000

    * reader sqlite
    git-svn-id: https://svn.intellagence.eu/patchwork@1349 28de78d2-576f-9a45-b889-8b31570dbf8a

--- variations/pStudio/class/agent/pStudioWidget/reader/sqlite.php
+++ variations/pStudio/class/agent/pStudioWidget/reader/sqlite.php
@@ -2,63 +2,26 @@
 
 class extends agent_pStudioWidget_reader
 {
-	protected
+	function control()
+	{
+		$this->get->__0__ = 'sqlite/' . $this->get->__0__;
 
-	//$language = 'sql',
-	$template = 'pStudioWidget/reader/sqlite';
-
-        //TODO si c'est un sqlite et que sqlite est présent alors
-        //affichage du contenu de la base?
-
-	function control(){}
+		parent::control();
+	}
 
 	function compose($o)
 	{
-		$o = agent_pStudioWidget_reader::compose($o);
+		$db = new SQLiteDatabase($this->realpath, 0444, $o->error_msg);
 
-		$dbname = explode('.', $this->get->__0__);
-		$dbname = array_reverse($dbname);
-		$o->dbname = $dbname[0];
+		$sql = "SELECT name, type FROM sqlite_master
+			WHERE type IN ('table', 'view')
+			ORDER BY name";
+		$tables = $db->arrayQuery($sql, SQLITE_ASSOC);
 
-		// manque resolvePath....
-		$db = new SQLiteDatabase($o->dbname,0666,$error);
-		//$db->sqlite_open($o->dbname,0666,$error);
-
-//		$sql = "SELECT name FROM sqlite_master WHERE (type = 'table')";
-
-		$sql = "SELECT * FROM sqlite_master WHERE (type = 'table')";
-E($db);
-		$tables = array();
-		while ($r =  $db->query($sql)->fetch())
-		{
-			$tables[] = $r;
-			E($r);
-		}
-
-E($tables);
-
+		E($tables);
 
 		$o->tables = new loop_array($tables, 'filter_rawArray');
-E($o->tables);
-
 
 		return $o;
 	}
-
-
-	function sqlite_list_tables ($db)
-	{
-		$tables = array ();
-
-		$sql = "SELECT name FROM sqlite_master WHERE (type = 'table')";
-		while($db->query($sql)->fetchObject())
-		{
-			$tables[] = sqlite_fetch_single($res);
-		}
-
-		return $tables;
-	}
-
-
 }
-
