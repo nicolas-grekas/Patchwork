7e3214ceff98be0470125d863646d21acacd57e5
Nicolas Grekas <nicolas.grekas@gmail.com>
2008-02-18 15:44:59 +0000

    * some work on pStudio SQLite reader

--- variations/pStudio/class/agent/pStudioWidget/reader/sqlite.php
+++ variations/pStudio/class/agent/pStudioWidget/reader/sqlite.php
@@ -6,17 +6,16 @@ class extends agent_pStudioWidget_reader
 		'path:c',
 		'low:i' => false,
 		'high:i' => PATCHWORK_PATH_LEVEL,
-		'table:c',
+		'sql:t',
 		'start:i:0' => 0,
 		'length:i:1' => 25,
-		'q:c',
 	);
 
 	function compose($o)
 	{
 		$db = new SQLiteDatabase($this->realpath, 0666, $o->error_msg);
 
-		if (!$this->get->table)
+		if (!$this->get->sql)
 		{
 			$sql = "SELECT name, type
 				FROM sqlite_master
@@ -28,9 +27,11 @@ class extends agent_pStudioWidget_reader
 		}
 		else
 		{
-			$table = strtr($this->get->table, '[]', '  ');
+			// TODO:
+			// - security considerations,
+			// - better LIMIT handling
+			$sql = "{$this->get->sql} LIMIT {$this->get->start}, {$this->get->length}";
 
-			$sql = "SELECT * FROM [{$table}] LIMIT {$this->get->start}, {$this->get->length}";
 			if ($rows = $db->arrayQuery($sql, SQLITE_ASSOC))
 			{
 				$o->fields = new loop_array(array_keys($rows[0]));
@@ -39,29 +40,6 @@ class extends agent_pStudioWidget_reader
 				$o->length = $this->get->length;
 			}
 		}
-
-        // Définition du formulaire de recherche
-        $form = new pForm($o);
-
-        $form->add('textarea', 'q');
-		$send=$form->add('submit','send');
-		$send->add('q', '', '');
-
-		if ($send->isOn())
-		{
-			$data = $send->getData();
-			$sql = sqlite_escape_string($data['q']);
-
-E($sql);
-			if ($rows = $db->arrayQuery($sql, SQLITE_ASSOC))
-			{
-E($rows);
-				$o->results = new loop_array($rows, 'filter_rawArray');
-				$o->fields = new loop_array(array_keys($rows[0]));
-				$o->rows = new loop_array($rows, array($this, 'filterRow'));
-			}
-		}
-
 
 		return $o;
 	}
