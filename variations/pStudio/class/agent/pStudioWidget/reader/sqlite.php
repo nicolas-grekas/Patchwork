e2bb848f75a318214713ad5f1a6ad87c1b711f38
nicolas.grekas <nicolas.grekas@28de78d2-576f-9a45-b889-8b31570dbf8a>
2008-02-20 19:11:04 +0000

    * pStudio sqlite reader check for read only queries
    git-svn-id: https://svn.intellagence.eu/patchwork@1379 28de78d2-576f-9a45-b889-8b31570dbf8a

--- variations/pStudio/class/agent/pStudioWidget/reader/sqlite.php
+++ variations/pStudio/class/agent/pStudioWidget/reader/sqlite.php
@@ -2,9 +2,7 @@
 
 class extends agent_pStudioWidget_reader
 {
-	public
-
-	$get = array(
+	public $get = array(
 		'path:c',
 		'low:i' => false,
 		'high:i' => PATCHWORK_PATH_LEVEL,
@@ -17,7 +15,28 @@ class extends agent_pStudioWidget_reader
 	{
 		$db = new SQLiteDatabase($this->realpath, 0666, $o->error_msg);
 
-		if (!$this->get->sql)
+		if ($this->get->sql)
+		{
+			$a = new geshi(trim($this->get->sql), 'sql');
+			$a->set_encoding('UTF-8');
+			$o->sql = $a->parse_code();
+
+			if (self::isReadOnlyQuery($db, $this->get->sql, $o->error_msg))
+			{
+				$sql = "{$this->get->sql}\n LIMIT {$this->get->start}, {$this->get->length}";
+				$rows = @$db->arrayQuery($sql, SQLITE_ASSOC);
+
+				if (false !== $rows)
+				{
+					$o->fields = new loop_array(array_keys($rows[0]));
+					$o->rows = new loop_array($rows, array($this, 'filterRow'));
+					$o->start = $this->get->start;
+					$o->length = $this->get->length;
+				}
+				else $o->error_msg = sqlite_error_string($db->lastError());
+			}
+		}
+		else
 		{
 			$sql = "SELECT name, type
 				FROM sqlite_master
@@ -26,33 +45,6 @@ class extends agent_pStudioWidget_reader
 			$tables = $db->arrayQuery($sql, SQLITE_ASSOC);
 			$o->tables = new loop_array($tables, 'filter_rawArray');
 		}
-		else
-		{
-			// TODO:
-			// - security considerations,	=>	(pStudio::isAuthQuery($this->get->path, $db, $sql)
-			// - better LIMIT handling		=>	limit for select request only
-			$sql_explode = explode(";", $this->get->sql);
-			if (count($sql_explode)>1) $sql = "{$sql_explode[0]}";
-			else $sql = "{$this->get->sql}";
-
-			if ($this->isReadOnlyQuery($this->get->path, $db, $sql))
-			{
-				if (preg_match('/\bselect\b/i', $sql) && !preg_match('/\blimit\b/i', $sql)) $sql = $sql . " LIMIT {$this->get->start}, {$this->get->length}";
-
-				if ($rows = @$db->arrayQuery($sql, SQLITE_ASSOC))
-				{
-					$o->fields = new loop_array(array_keys($rows[0]));
-					$o->rows = new loop_array($rows, array($this, 'filterRow'));
-					$o->start = $this->get->start;
-					$o->length = $this->get->length;
-				}
-				if ($db->lastError()) $o->errorSQLite = sqlite_error_string($db->lastError());
-
-				$a = new geshi($this->get->sql, 'sql');
-				$a->set_encoding('UTF-8');
-				$o->query = $a->parse_code($sql);
-			}
- 		}
 
 		return $o;
 	}
@@ -67,16 +59,10 @@ class extends agent_pStudioWidget_reader
 	}
 
 
-	public function isReadOnlyQuery($path, $db, $sql)
+	protected static function isReadOnlyQuery($db, $sql, &$error_msg)
 	{
-		$explain = @$db->arrayQuery("EXPLAIN " . $sql, SQLITE_ASSOC);
-		if (!is_array($explain)) return false;
+		$sql = str_replace(';', ';EXPLAIN ', $sql);
 
-		foreach ($explain as $rx)
-		{
-			if($rx['opcode'] == 'OpenWrite') return false;
-		}
-
-		return true;
+		return $db->queryExec("EXPLAIN {$sql}", $error_msg) && $db->queryExec("EXPLAIN SELECT 1 FROM ({$sql}\n)", $error_msg);
 	}
 }
