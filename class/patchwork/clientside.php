ef03224345b2824840d07265e8942f2eb2fd68cb
nicolas.grekas <nicolas.grekas@28de78d2-576f-9a45-b889-8b31570dbf8a>
2008-03-07 13:51:37 +0000

    - fixes (and disabled IE7, it needs fixes to be enabled by default)
    git-svn-id: https://svn.intellagence.eu/patchwork@1400 28de78d2-576f-9a45-b889-8b31570dbf8a

--- class/patchwork/clientside.php
+++ class/patchwork/clientside.php
@@ -40,7 +40,7 @@ class extends patchwork
 		}
 		else $b = '0';
 
-		$lang = !$lang || '__' === $lang ? '' : " xml:lang=\"{$lang}\" lang=\"{$lang}\"";
+		$lang = $lang ? " xml:lang=\"{$lang}\" lang=\"{$lang}\"" : '';
 
 		echo $a =<<<EOHTML
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
