2feceaf209f8c1ab32ab128c22d44fbb31608d2e
nicolas.grekas <nicolas.grekas@28de78d2-576f-9a45-b889-8b31570dbf8a>
2008-10-13 08:06:29 +0000

    git-svn-id: https://svn.intellagence.eu/patchwork@1569 28de78d2-576f-9a45-b889-8b31570dbf8a

---
+++ class/patchwork/exception/redirection.php
@@ -0,0 +1,51 @@
+<?php /*********************************************************************
+ *
+ *   Copyright : (C) 2007 Nicolas Grekas. All rights reserved.
+ *   Email     : p@tchwork.org
+ *   License   : http://www.gnu.org/licenses/agpl.txt GNU/AGPL
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as
+ *   published by the Free Software Foundation, either version 3 of the
+ *   License, or (at your option) any later version.
+ *
+ ***************************************************************************/
+
+
+class patchwork_exception_redirection extends Exception
+{
+	protected $url;
+
+	function __construct($url)
+	{
+		$url = (string) $url;
+		$url = '' === $url ? '' : (preg_match("'^([^:/]+:/|\.+)?/'", $url) ? $url : (patchwork::__BASE__() . ('index' === $url ? '' : $url)));
+
+		if ('.' === substr($url, 0, 1)) W('Current redirection behaviour with relative URLs may change in a future version of Patchwork. As long as this notice appears, using relative URLs is strongly discouraged.');
+
+		$this->url = $url;
+	}
+
+	function redirect()
+	{
+		patchwork::disable();
+
+		$url = $this->url;
+
+		if (PATCHWORK_DIRECT)
+		{
+			$url = 'location.replace(' . ('' !== $url
+				? "'" . addslashes($url) . "'"
+				: 'location')
+			. ')';
+
+			header('Content-Length: ' . strlen($url));
+			echo $url;
+		}
+		else
+		{
+			header('HTTP/1.1 302 Found');
+			header('Location: ' . ('' !== $url ? $url : $_SERVER['REQUEST_URI']));
+		}
+	}
+}
