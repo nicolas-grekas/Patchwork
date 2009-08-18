317d12a95cc3161cfae51dc85352b4d3049cb78b
Nicolas Grekas <nicolas.grekas@gmail.com>
2009-08-18 18:30:08 +0000

    * preprocessor refactoring * bootstrapping cleanup

---
+++ class/patchwork/preprocessor/t.php
@@ -0,0 +1,26 @@
+<?php /*********************************************************************
+ *
+ *   Copyright : (C) 2007 Nicolas Grekas. All rights reserved.
+ *   Email     : p@tchwork.org
+ *   License   : http://www.gnu.org/licenses/agpl.txt GNU/AGPL
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as
+ *   published by the Free Software Foundation, either version 3 of the
+ *   License, or (at your option) any later version.
+ *
+ ***************************************************************************/
+
+
+class extends patchwork_preprocessor_bracket
+{
+	function filterBracket($type, $token)
+	{
+		if ('.' === $type) patchwork_preprocessor::error(
+			"Usage of T() is potentially divergent, please use sprintf() instead of string concatenation.",
+			$this->preproc->source, $this->preproc->line
+		);
+
+		return $token;
+	}
+}
