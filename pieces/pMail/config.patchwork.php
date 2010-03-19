e00648abb1c13c6908191dc42e370dbb24f257d2
Nicolas Grekas <nicolas.grekas@gmail.com>
2010-03-19 18:27:19 +0000

    * removed pMail.from in favor of pMail.sender

--- pieces/pMail/config.patchwork.php
+++ pieces/pMail/config.patchwork.php
@@ -2,7 +2,7 @@
 
 $CONFIG += array(
 
-#	'pMail.from'        => '',          // Default value for From header
+#	'pMail.sender'      => '',          // Default value for From and Return-Path headers
 #	'pMail.debug_email' => 'webmaster', // Used by pMail in test mode
 #	'pMail.backend'     => 'mail',      // See PEAR's Mail_mime constructor
 #	'pMail.options'     => '',          // See PEAR's Mail_mime constructor
