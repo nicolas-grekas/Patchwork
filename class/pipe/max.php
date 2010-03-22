ee29b71c784bcc882c0dea2d142c0a0049277e5d
Nicolas Grekas <nicolas.grekas@gmail.com>
2010-03-22 16:53:21 +0000

    * add the max pipe

---
+++ class/pipe/max.php
@@ -0,0 +1,32 @@
+<?php /*********************************************************************
+ *
+ *   Copyright : (C) 2010 Nicolas Grekas. All rights reserved.
+ *   Email     : p@tchwork.org
+ *   License   : http://www.gnu.org/licenses/agpl.txt GNU/AGPL
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as
+ *   published by the Free Software Foundation, either version 3 of the
+ *   License, or (at your option) any later version.
+ *
+ ***************************************************************************/
+
+
+class
+{
+	static function php($a, $b)
+	{
+		return p::string($a) < p::string($b) ? $b : $a;
+	}
+
+	static function js()
+	{
+		?>/*<script>*/
+
+P$max = function($a, $b)
+{
+	return num(str($a), 1) < num(str($b), 1) ? $b : $a;
+}
+
+<?php	}
+}
