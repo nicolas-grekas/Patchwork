29188cc53f4bf83c317e7f466a51631d1efa376c
Nicolas Grekas <nicolas.grekas@gmail.com>
2010-04-15 20:31:34 +0200

    * moved pStudio to a git submodule

--- variations/pStudio/core/class/agent/pStudio/opener/ico.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class extends agent_pStudio_opener_gif
-{
-	protected $rawContentType = 'image/vnd.microsoft.icon';
-}
-
--- variations/pStudio/core/class/agent/pStudio/opener/png.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class extends agent_pStudio_opener_gif
-{
-	protected $rawContentType = 'image/png';
-}
