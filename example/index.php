c4f4e65c2774de2c0c159b9226f8239013345a89
nicolas.grekas <nicolas.grekas@intellagence.eu>
2010-05-03 14:08:47 +0200

    * update apache integration rules

--- example/index.php
+++ example/index.php
@@ -1,15 +1,12 @@
 <?php
 
-
 // Set the path to your patchwork application directory:
-$a = '../variations/hello';
-
-	define('PATCHWORK_BOOTPATH', $a);
-
+define('PATCHWORK_BOOTPATH', '../variations/hello');
 
 // Set the path to patchwork's bootstrapper.php file:
 $a = '../bootstrapper.php';
 
-	$a = include file_exists(PATCHWORK_BOOTPATH . '/.patchwork.php')
-		? PATCHWORK_BOOTPATH . '/.patchwork.php' : $a;
-	$a || die("Failed inclusion of patchwork's bootstrapper.php");
+
+$a = include file_exists(PATCHWORK_BOOTPATH . '/.patchwork.php')
+	? PATCHWORK_BOOTPATH . '/.patchwork.php' : $a;
+$a || die("Failed inclusion of patchwork's bootstrapper.php");
