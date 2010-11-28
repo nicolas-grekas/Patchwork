7fe4612d85e720e84a8e76b35eec87485efff6fc
Nicolas Grekas <nicolas.grekas@gmail.com>
2010-11-29 00:48:28 +0100

    * a step towards NS aware function aliasing

--- class/patchwork/alias.php
+++ class/patchwork/alias.php
@@ -4,7 +4,21 @@ class patchwork_alias
 {
 	static function resolve($c)
 	{
-		return is_string($c) && function_exists('__patchwork_' . $c) ? '__patchwork_' . $c : $c;
+		if (is_string($c) && isset($c[0]))
+		{
+			if ('\\' === $c[0])
+			{
+				if (!isset($c[1]) || '\\' === $c[1]) return $c;
+				$c = substr($c, 1);
+			}
+
+			if (function_exists('__patchwork_' . strtr($c, '\\', '_')))
+			{
+				return '__patchwork_' . strtr($c, '\\', '_');
+			}
+		}
+
+		return $c;
 	}
 
 	static function scopedResolve($c, &$v)
