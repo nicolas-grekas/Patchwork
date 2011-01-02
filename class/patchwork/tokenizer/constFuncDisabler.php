6492e36f61b968b4a8d79e7452224bf244cc8cd7
Nicolas Grekas <nicolas.grekas@gmail.com>
2011-01-02 23:59:48 +0100

    * disabled namespaced functions and constants
    Promote static methods and class consts instead.
    This is necessary to resolve function calls at compile time,
    then allowing function aliasing. This also simplify code packaging
    by removing the need for any function and const loading scheme.

---
+++ class/patchwork/tokenizer/constFuncDisabler.php
@@ -0,0 +1,50 @@
+<?php /*********************************************************************
+ *
+ *   Copyright : (C) 2011 Nicolas Grekas. All rights reserved.
+ *   Email     : p@tchwork.org
+ *   License   : http://www.gnu.org/licenses/agpl.txt GNU/AGPL
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as
+ *   published by the Free Software Foundation, either version 3 of the
+ *   License, or (at your option) any later version.
+ *
+ ***************************************************************************/
+
+
+class patchwork_tokenizer_constFuncDisabler extends patchwork_tokenizer
+{
+	protected
+
+	$callbacks = array('tagOpenTag' => T_SCOPE_OPEN),
+	$depends   = array(
+		'patchwork_tokenizer_scoper',
+		'patchwork_tokenizer_namespaceInfo',
+	);
+
+
+	function tagOpenTag(&$token)
+	{
+		if (T_NAMESPACE === $this->scope->type && '\\' !== $this->namespace)
+		{
+			$this->register($this->callbacks = array(
+				'tagConstFunc' => array(T_NAME_FUNCTION, T_NAME_CONST)
+			));
+
+			return 'tagScopeClose';
+		}
+	}
+
+	function tagScopeClose(&$token)
+	{
+		$this->unregister();
+	}
+
+	function tagConstFunc(&$token)
+	{
+		if (T_CLASS !== $this->scope->type)
+		{
+			$this->setError("Namespaced functions and constants have been deprecated. Please use static methods and class constants.");
+		}
+	}
+}
