0febe3bb6a590cc3fccee7fd0d28fa25e593aad2
nicolas.grekas <nicolas.grekas@intellagence.eu>
2011-03-24 01:20:36 +0100

    - fix

--- common.php
+++ common.php
@@ -68,6 +68,48 @@ function ini_get_bool($a)
 }
 
 
+// Runtime function aliasing: private use for the preprocessor
+
+function patchwork_alias_resolve($c)
+{
+    if (is_string($c) && isset($c[0]))
+    {
+        if ('\\' === $c[0])
+        {
+            if (empty($c[1]) || '\\' === $c[1]) return $c;
+            $c = substr($c, 1);
+        }
+
+        if (function_exists('__patchwork_' . strtr($c, '\\', '_')))
+            return '__patchwork_' . strtr($c, '\\', '_');
+
+/**/    if (PHP_VERSION_ID < 50300)
+            $c = strtr($c, '\\', '_');
+
+/**/    if (PHP_VERSION_ID < 50203)
+            strpos($c, '::') && $c = explode('::', $c, 2);
+    }
+    else
+    {
+/**/    if (PHP_VERSION_ID < 50300)
+/**/    {
+            if (is_array($c) && isset($c[0]) && is_string($c[0]))
+                $c[0] = strtr($c[0], '\\', '_');
+/**/    }
+    }
+
+    return $c;
+}
+
+function patchwork_alias_resolve_ref($c, &$v)
+{
+    $v = patchwork_alias_resolve($c);
+/**/if (PHP_VERSION_ID < 50203)
+        is_array($v) && is_string($c) && $v = implode('', $v);
+    return "\x9D";
+}
+
+
 // If realpath or getcwd are bugged, enable a workaround
 
 /**/$a = function_exists('realpath') ? @realpath('.') : false;
