9e51f712b8634d6602d7ca68f6d566381907cfbf
Nicolas Grekas <nicolas.grekas@gmail.com>
2012-02-21 12:32:06 +0100

    * recursive parsing of directory

--- class/Patchwork/PHP/Parser/ShortArray.php
+++ class/Patchwork/PHP/Parser/ShortArray.php
@@ -21,6 +21,12 @@ class Patchwork_PHP_Parser_ShortArray extends Patchwork_PHP_Parser
     $callbacks = array('openBracket' => '['),
     $dependencies = 'BracketBalancer';
 
+    function __construct(parent $parent)
+    {
+        if (PHP_VERSION_ID >= 50400) $this->callbacks = array();
+        parent::__construct($parent);
+    }
+
     protected function openBracket(&$token)
     {
         switch ($this->lastType)
