f40aa848d660db7e61245f652da5b42eaaf78c0b
Nicolas Grekas <nicolas.grekas@gmail.com>
2012-09-12 08:43:38 +0200

    take advantage of JsonDumpIterator

--- core/superloader/class/Patchwork/ErrorHandler.php
+++ core/superloader/class/Patchwork/ErrorHandler.php
@@ -49,7 +49,6 @@ class ErrorHandler extends PHP\ErrorHandler
         // http://bugs.php.net/42098 workaround
         class_exists('Patchwork\Logger') || eval(';') || __autoload('Patchwork\Logger');
         $l = new Logger(self::$logStream, $_SERVER['REQUEST_TIME_FLOAT']);
-        $l->lineFormat = sprintf('%010d', substr(mt_rand(), -10)) . ": %s";
 
         return $this->logger = $l;
     }
--- pieces/agent-controller/class/Patchwork/Debugger.php
+++ pieces/agent-controller/class/Patchwork/Debugger.php
@@ -130,43 +130,31 @@ class Debugger extends p
             foreach (scandir(PATCHWORK_ZCACHE) as $log)
                 if ('.log' === substr($log = PATCHWORK_ZCACHE . $log, -4))
                     if (rename($log, $log .= '~'))
-                        ($h = fopen($log, 'rb')) ? $handlers[$log] = $h : unlink($log);
+                        ($h = fopen($log, 'rb')) ? $handlers[$log] = new p\PHP\JsonDumpIterator($h) : unlink($log);
 
             $count = 0;
 
             foreach ($handlers as $log => $h)
             {
-                if (false === $next_line = fgets($h)) ++$count;
-                else while (false !== $line = $next_line)
+                try
                 {
-                    $next_line = fgets($h);
-
-                    self::parseLine($line, $next_line);
-
-                    for (;;)
+                    foreach ($h as $j)
                     {
-                        if (false !== $line = reset(self::$buffer))
-                        {
-                            echo implode('', $line);
+                        echo '<script>patchworkConsole.log(',
+                            $h->jsonStr($j['type']), ',',
+                            $j['json'], ',',
+                            $h->jsonStr(substr(md5($log), -10)),
+                            ')</script>', "\n";
+                        ob_flush();
+                        flush();
 
-                            if ($line && false === end($line))
-                            {
-                                unset(self::$buffer[key(self::$buffer)]);
-
-                                ob_flush();
-                                flush();
-
-                                if (connection_aborted()) break 4;
-                                else continue;
-                            }
-                            else
-                            {
-                                self::$buffer[key(self::$buffer)] = array();
-                            }
-                        }
-
-                        break;
+                        if (connection_aborted()) break 3;
                     }
+
+                    ++$count;
+                }
+                catch (p\PHP\JsonDumpIteratorException $h)
+                {
                 }
             }
 
@@ -175,7 +163,7 @@ class Debugger extends p
             usleep(150000);
         }
 
-        foreach ($handlers as $log => $h) fclose($h) + unlink($log);
+        foreach ($handlers as $log => $h) fclose($h->getStream()) + unlink($log);
 
         ?>
 </div>
@@ -186,155 +174,5 @@ for (i in b) patchworkConsole.log("client-dump", b[i]);
 parent.E.buffer = [];
 </script>
 <?php
-    }
-
-    protected static function parseLine($line, $next_line)
-    {
-        if ('' !== $line && '[' === $line[0] && (false !== $offset = strpos($line, ']')) && '] PHP ' === substr($line, $offset, 6))
-        {
-            static $raw_token; empty($raw_token) && $raw_token = substr(mt_rand(), -10);
-
-            $offset += 6;
-
-            if (preg_match("' on line \d+$'", $line))
-            {
-                $line = self::parseRawError($line, $offset);
-                $line = array(
-                    '*** php-error ***',
-                    '{',
-                    '  "time": ' . p\PHP\JsonDumper::get($line['date']) . ',',
-                    '  "data": {',
-                    '    "mesg": ' . p\PHP\JsonDumper::get($line['message']) . ',',
-                    '    "code": ' . p\PHP\JsonDumper::get("{$line['type']} {$line['file']}:{$line['line']}") . ',',
-                    '    "level": ' . p\PHP\JsonDumper::get(constant($line['type']) . '/-1'),
-                );
-
-                if ("Stack trace:" === substr(rtrim($next_line), $offset))
-                {
-                    $line[count($line) - 1] .= ',';
-                }
-            }
-            else
-            {
-                // Xdebug inserted stack trace
-
-                $line = substr(rtrim($line), $offset);
-
-                if ("Stack trace:" === $line)
-                {
-                    $line = array('    "trace": {');
-                }
-                else
-                {
-                    // TODO: more extensive parsing of dumped arguments using token_get_all() / client-side parsing?
-
-                    preg_match("' +(\d+)\. (.+?)\((.*)\) (.*)$'", $line, $line);
-
-                    $line = array(
-                        '      "' . $line[1] . '": {',
-                        '        "call": ' . p\PHP\JsonDumper::get("{$line[2]}() {$line[4]}") . ('' !== $line[3] ? ',' : ''),
-                        '' !== $line[3] ? '        "args": ' . p\PHP\JsonDumper::get($line[3]) : null,
-                        '      }'
-                    );
-
-                    if ('[' !== $next_line[0] || (false === $offset = strpos($next_line, ']')) || '] PHP ' !== substr($next_line, $offset, 6) || preg_match("' on line \d+$'", $next_line))
-                    {
-                        $line[] = '    }';
-                    }
-                    else
-                    {
-                        $line[count($line) - 1] .= ',';
-                    }
-                }
-            }
-
-            if ('[' !== $next_line[0] || (false === $offset = strpos($next_line, ']')) || '] PHP ' !== substr($next_line, $offset, 6) || preg_match("' on line \d+$'", $next_line))
-            {
-                $line[] = '  }';
-                $line[] = '}    ';
-                $line[] = '***';
-            }
-
-            foreach ($line as $line) null !== $line && self::htmlDumpLine("{$raw_token}: {$line}\n");
-        }
-        else
-        {
-            self::htmlDumpLine($line);
-        }
-    }
-
-    protected static function parseRawError($a, $offset)
-    {
-        $b = strpos($a, ':', $offset + 1);
-        $b = array(
-            'date' => substr($a, 1, $offset - 7),
-            'type' => substr($a, $offset, $b - $offset),
-            'message' => rtrim(substr($a, $b+3)),
-            'file' => '',
-            'line' => 0,
-        );
-
-        $b['date'] = date('c', strtotime($b['date']));
-
-        static $msg_map = array(
-            'Notice' => 'E_NOTICE',
-            'Warning' => 'E_WARNING',
-            'Deprecated' => 'E_DEPRECATED',
-            'Fatal error' => 'E_ERROR',
-            'Parse error' => 'E_PARSE',
-            'Strict standards' => 'E_STRICT', // From Xdebug
-            'Strict Standards' => 'E_STRICT',
-            'Catchable fatal error' => 'E_RECOVERABLE_ERROR',
-        );
-
-        if (isset($msg_map[$b['type']]))
-        {
-            $b['type'] = $msg_map[$b['type']];
-        }
-
-        if (preg_match('/^(.*) in (.*) on line (\d+)$/s', $b['message'], $a))
-        {
-            $b['message'] = $a[1];
-            $b['file'] = $a[2];
-            $b['line'] = $a[3];
-        }
-
-        return $b;
-    }
-
-    protected static function htmlDumpLine($a)
-    {
-        list($token, $a) = explode(': ', rtrim($a) , 2);
-
-        $t = $token . '-';
-
-        if ('*** ' === substr($a, 0, 4))
-        {
-            $t .= empty(self::$bufferDepth[$token])
-                ? self::$bufferDepth[$token] = 1
-                : ++self::$bufferDepth[$token];
-
-            self::$buffer[$t][] = '<script>patchworkConsole.log(' . p\PHP\JsonDumper::get(substr($a, 4, -4)) . ',';
-        }
-        else if (!empty(self::$bufferDepth[$token]))
-        {
-            $t .= self::$bufferDepth[$token];
-
-            if ('***' === $a)
-            {
-                self::$buffer[$t][] = ',' . p\PHP\JsonDumper::get($token) . ')</script>';
-                self::$buffer[$t][] = false;
-
-                if (0 === --self::$bufferDepth[$token]) unset(self::$bufferDepth[$token]);
-            }
-            else
-            {
-                self::$buffer[$t][] = $a . "\n";
-            }
-        }
-        else
-        {
-            user_error('Invalid debug stream: this point has not reason to be reached', E_USER_WARNING);
-        }
     }
 }
