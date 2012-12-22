c674d293f1fbdc3eea7c8afbe90e3372bedf9630
Nicolas Grekas <nicolas.grekas@gmail.com>
2012-12-22 18:15:45 +0100

    new StaticInit and Constructor4to5 parsers

---
+++ class/Patchwork/PHP/Parser/Constructor4to5.php
@@ -0,0 +1,77 @@
+<?php // vi: set fenc=utf-8 ts=4 sw=4 et:
+/*
+ * Copyright (C) 2012 Nicolas Grekas - p@tchwork.com
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the (at your option):
+ * Apache License v2.0 (http://apache.org/licenses/LICENSE-2.0.txt), or
+ * GNU General Public License v2.0 (http://gnu.org/licenses/gpl-2.0.txt).
+ */
+
+namespace Patchwork\PHP\Parser;
+
+/**
+ * The Constructor5to4 parser renames PHP4-style constructor to PHP5's __construct().
+ */
+class Constructor4to5 extends InvokeShim
+{
+    public
+
+    $targetPhpVersionId = 0;
+
+    protected
+
+    $namespace,
+    $dependencies = array('ClassInfo' => array('class', 'namespace', 'scope'));
+
+
+    protected function tagClassOpen(&$token)
+    {
+        $this->namespace or parent::tagClassOpen($token);
+    }
+
+    protected function tagFunctionName(&$token)
+    {
+        if ('&' === $token[0]) return;
+        $this->unregister(__FUNCTION__);
+        if (T_STRING !== $token[0]) return;
+
+        if (0 === strcasecmp($token[1], '__construct'))
+        {
+            $this->signature = '';
+            $this->unregister($this->callbacks);
+        }
+        else if (empty($this->class->suffix)) {}
+        else if (0 === strcasecmp($token[1], $this->class->nsName))
+        {
+            $this->signature = $token[1];
+            $token[1] = '__construct';
+            $this->register('catchSignature');
+        }
+        else if (0 === strcasecmp($token[1], $this->class->nsName . $this->class->suffix))
+        {
+            $this->setError("Constructor collision: __construct() must be defined and before {$token[1]}() in class {$this->class->nsName}", E_USER_ERROR);
+        }
+    }
+
+    protected function tagClassClose(&$token)
+    {
+        if ('' !== $this->signature)
+        {
+            $n = $this->targetPhpVersionId < 50300 ? strtr($this->class->nsName, '\\', '_') : $this->class->nsName;
+
+            $token[1] = 'function ' . $this->signature . '{'
+                . 'if(' . count($this->arguments) . '<func_num_args()){'
+                .   '${""}=array(' . implode(',', $this->arguments) . ')+func_get_args();'
+                .   'call_user_func_array(array("' . $n . '","__construct"),${""});'
+                . '}else{'
+                .   $n . '::__construct(' . str_replace('&', '', implode(',', $this->arguments)) . ');'
+                . '}}'
+                . $token[1];
+
+            $this->signature = '';
+        }
+
+        parent::tagClassClose($token);
+    }
+}
---
+++ class/Patchwork/PHP/Parser/StaticInit.php
@@ -0,0 +1,84 @@
+<?php // vi: set fenc=utf-8 ts=4 sw=4 et:
+/*
+ * Copyright (C) 2012 Nicolas Grekas - p@tchwork.com
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the (at your option):
+ * Apache License v2.0 (http://apache.org/licenses/LICENSE-2.0.txt), or
+ * GNU General Public License v2.0 (http://gnu.org/licenses/gpl-2.0.txt).
+ */
+
+namespace Patchwork\PHP\Parser;
+
+use Patchwork\PHP\Parser;
+
+/**
+ * The StaticInit parser adds __init() and __free() magic methods for static class initialization and freeing.
+ */
+class StaticInit extends Parser
+{
+    protected
+
+    $tag = "\x9D",
+    $init,
+    $free,
+    $callbacks = array('tagClassOpen' => T_SCOPE_OPEN),
+
+    $class, $scope,
+    $dependencies = array('ClassInfo' => array('class', 'scope'));
+
+
+    protected function tagClassOpen(&$token)
+    {
+        if (T_CLASS === $this->scope->type)
+        {
+            $this->unregister($this->callbacks);
+            $this->init = $this->free = (int) empty($this->class->extendsSelf);
+            $this->register(array(
+                'tagFunction'   => T_FUNCTION,
+                'tagClassClose' => T_BRACKET_CLOSE,
+            ));
+        }
+    }
+
+    protected function tagFunction(&$token)
+    {
+        if (T_CLASS === $this->scope->type)
+        {
+            $t = $this->getNextToken($i);
+            if ('&' === $t[0]) $t = $this->getNextToken($i);
+
+            if (T_STRING === $t[0]) switch (strtolower($t[1]))
+            {
+                case '__init': $this->init = 2; break;
+                case '__free': $this->free = 2; break;
+            }
+        }
+    }
+
+    protected function tagClassClose(&$token)
+    {
+        $this->unregister(array('tagFunction' => T_FUNCTION, 'tagClassClose' => T_BRACKET_CLOSE));
+        $this->register($this->callbacks);
+
+        $class = strtolower(strtr($this->class->nsName, '\\', '_'));
+        $d = "\\Patchwork\\ShutdownHandler::\$destructors[]='{$class}';";
+        $this->targetPhpVersionId < 50300 && $d[0] = ' ';
+
+        $this->init && $token[1] = "const i{$this->tag}=" . (2 === $this->init ? "'{$class}';" : "'';static function __init(){}") . $token[1];
+        $this->free && $token[1] = "const f{$this->tag}=" . (2 === $this->free ? "'{$class}';" : "'';static function __free(){}") . $token[1];
+
+        if (isset($this->class->isTop) && false === $this->class->isTop) return;
+
+        if ($this->class->extends)
+        {
+            1 !== $this->init && $token[1] .= "if('{$class}'==={$this->class->name}::i{$this->tag}){$this->class->name}::__init();";
+            1 !== $this->free && $token[1] .= "if('{$class}'==={$this->class->name}::f{$this->tag}){$d}";
+        }
+        else
+        {
+            2 === $this->init&& $token[1] .= "{$this->class->name}::__init();";
+            2 === $this->free&& $token[1] .= $d;
+        }
+    }
+}
