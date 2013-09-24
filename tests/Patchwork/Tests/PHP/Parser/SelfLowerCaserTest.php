c3ccc46ff4bf997810183ccc1323dc525dd37a78
Nicolas Grekas <nicolas.grekas@gmail.com>
2013-09-24 21:22:51 +0000

    SelfLowerCaserTest

---
+++ tests/Patchwork/Tests/PHP/Parser/SelfLowerCaserTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Patchwork\Tests\PHP;
+
+use Patchwork\PHP\Parser;
+
+class SelfLowerCaserTest extends \PHPUnit_Framework_TestCase
+{
+    protected function getParser($dump = false)
+    {
+        $p = $dump ? new Parser\Dumper : new Parser;
+
+        $this->targetPhpVersionId = 50400;
+
+        $p = new Parser\SelfLowerCaser($p);
+
+        return $p;
+    }
+
+    function testShortOpenEcho()
+    {
+        $parser = $this->getParser();
+
+        $in  = '<?php SelF; ParenT;';
+        $out = '<?php self; parent;';
+
+        $this->assertSame( $out, $parser->parse($in) );
+        $this->assertSame( array(), $parser->getErrors() );
+    }
+}
