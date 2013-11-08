02b7de250ad939ce9c73274b7f048cce191a7317
Nicolas Grekas <nicolas.grekas@gmail.com>
2013-11-08 10:58:29 +0000

    new CatchNotifier parser

---
+++ tests/Patchwork/Tests/PHP/Parser/CatchNotifierTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Patchwork\Tests\PHP;
+
+use Patchwork\PHP\Parser;
+
+class CatchNotifierTest extends \PHPUnit_Framework_TestCase
+{
+    protected function getParser($handler, $dump = false)
+    {
+        $p = $dump ? new Parser\Dumper : new Parser;
+
+        $p = new Parser\Normalizer($p);
+        $p = new Parser\StringInfo($p);
+        $p = new Parser\NamespaceInfo($p);
+        $p = new Parser\CatchNotifier($p, $handler);
+
+        return $p;
+    }
+
+    /**
+     * @dataProvider parserProvider
+     */
+    function testParser($handler, $in, $out, $errors = [])
+    {
+        $parser = $this->getParser($handler);
+
+        $in = $parser->parse('<?php ' . $in);
+        $in = substr($in, 6);
+        $in = trim(preg_replace('/  +/', ' ', $in));
+
+        $this->assertSame( $out, $in );
+        $this->assertSame( $errors, $parser->getErrors() );
+    }
+
+    function parserProvider()
+    {
+        return [
+            [
+                'handler' => false,
+                'in'  => 'try{}catch(\E $e){}}',
+                'out' => 'try{}catch(\E $e){\user_error(\'Caught \\\\E $e\');}}',
+            ],
+            [
+                'handler' => 'exh',
+                'in'  => 'try{}catch(\E $e){}}',
+                'out' => 'try{}catch(\E $e){\set_error_handler(\'exh\');\user_error(\'Caught \\\\E $e\');\restore_error_handler();}}',
+            ],
+            [
+                'handler' => ['e', 'xh'],
+                'in'  => 'try{}catch(\E $e){}}',
+                'out' => 'try{}catch(\E $e){\set_error_handler(array(\'e\',\'xh\'));\user_error(\'Caught \\\\E $e\');\restore_error_handler();}}',
+            ],
+        ];
+    }
+}
